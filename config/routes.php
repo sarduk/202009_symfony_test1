f1629339791bb51af363ae677cdc0713add22342
Rea Biagio <reabiagio@gmail.com>
2020-09-29 12:17:00 +0200

    route crea utenti

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,11 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
 
+    $routes->add('crea_utenti', '/crea-utenti/')
+        ->controller([App\Controller\ProvaInterazioneDoctrine::class, 'crea_utenti'])
+        ->methods(['GET', 'HEAD'])
+    ;
+
     $routes->add('prova_twig_paramname', '/prova-twig/{name}')
         ->controller([App\Controller\ProvaTwigController::class, 'prova_twig'])
         ->methods(['GET', 'HEAD'])
